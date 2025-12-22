155d5c68334b225a20d5e51e56919221772bef2f
SebastienWolfie <malseasers@gmail.com>
2025-12-22 14:25:01 +0000

    polyscore updatte

--- apiss/polyscore.js
+++ apiss/polyscore.js
@@ -1,51 +1,58 @@
-import Date from '../utils/Date';
-import { doc, collection, updateDoc, getDocs, getDoc, setDoc, deleteDoc } from 'firebase/firestore';
-import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { doc, collection, updateDoc, setDoc, getDoc, getDocs, deleteDoc } from 'firebase/firestore';
 import { db } from '../firebase';
+import Date from '../utils/Date';
 
 async function getAll() {
     return new Promise(async (resolve, reject) => {
-        const collectionRef = collection(db, 'polyscore');
-        const querySnapshot = await getDocs(collectionRef);
-  
-        let list = [];
-        querySnapshot.forEach((doc) => {
-          list.push(doc.data());
-        })
-        resolve(list);
+        try {
+            const collectionRef = collection(db, 'polyscore');
+            const querySnapshot = await getDocs(collectionRef);
+      
+            let list = [];
+            querySnapshot.forEach((doc) => {
+              // We include id from doc.id just in case the data doesn't have it
+              list.push({ id: doc.id, ...doc.data() });
+            })
+            resolve(list);
+        } catch (error) {
+            reject(error);
+        }
     })
 }
-
 
 async function getPolyscore(address) {
     return new Promise(async (resolve, reject) => {
-        const docRef = doc(db, 'polyscore', address)
-        const querySnapshot = await getDoc(docRef);
-  
-        resolve(querySnapshot.data());
+        try {
+            const docRef = doc(db, 'polyscore', address);
+            const querySnapshot = await getDoc(docRef);
+            resolve(querySnapshot.data());
+        } catch (error) {
+            reject(error);
+        }
     })
 }
 
-
 async function create(address, item) {
-    const res = await getPolyscore(address)
-    console.log('response', res)
+    // Check if exists to determine update vs create
+    const res = await getPolyscore(address);
     if (res) {
-        return update(address, item)
+        return update(address, item);
     }
+    
     return new Promise(async(resolve, reject) => {
         try {
-             const docRef = doc(db, 'polyscore', address)
+             const docRef = doc(db, 'polyscore', address);
              item.address = address;
-             item.dateCreated= new Date().toJSON();
+             item.id = address; // Ensure ID is consistent
+             item.dateCreated = new Date().toJSON();
 
              setDoc(docRef, item)
                  .then(result => {
                      resolve(item);
                  })
-                 .catch(reject)
+                 .catch(reject);
          } catch (error) {
-             reject(error)
+             reject(error);
          }
     })
 }
@@ -59,23 +66,28 @@ async function update(address, item) {
                 .then(result => {
                     resolve(item);
                 })
-                .catch(reject)
+                .catch(reject);
         } catch (error) {
-            reject(error)
+            reject(error);
         }
     })
 }
 
-
-
-
-
-  async function deletePolyscore(id) {
+async function deleteScore(id) {
     return new Promise((resolve, reject) => {
-        const docRef = doc(db, 'polyscore', id)
-        deleteDoc(docRef).then(resolve).catch(reject)
+        try {
+            const docRef = doc(db, 'polyscore', id);
+            deleteDoc(docRef).then(resolve).catch(reject);
+        } catch (error) {
+            reject(error);
+        }
     })
-  }
+}
 
-
-export { getAll, getPolyscore, create, update, deletePolyscore }
+export { 
+    getAll, 
+    getPolyscore, 
+    create, 
+    update, 
+    deleteScore 
+}
